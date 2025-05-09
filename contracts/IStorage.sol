54037fe89dd49c9deef4d4591345346f606129ee
Basil Foubert <basil.foubert@gmail.com>
2025-05-09 21:43:58 +0200

    create interface for storage.sol

---
+++ contracts/IStorage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface IStorage {
+    function creerStockage(int256 _temp) external;
+    function supprStockage(uint256 _stockageId) external;
+    function getNbProduits(uint256 _stockageId) external view returns (uint256);
+    function ajouterProduit(uint256 _stockageId, uint256 _produitId) external;
+    function retirerProduit(uint256 _stockageId, uint256 _produitId) external;
+}
